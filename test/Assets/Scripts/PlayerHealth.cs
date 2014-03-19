e09bb8b8bc80ffb8b1cb2c854a1c0daa8f61b3e8
skormsghost <skormsghost@hotmail.com>
2014-03-18 21:33:46 -0500

    Health and Death!
    Player now has health and can be damaged by enemies!  Easy to change how
    much health and damage the player takes.

--- test/Assets/Scripts/PlayerHealth.cs
+++ test/Assets/Scripts/PlayerHealth.cs
@@ -5,26 +5,26 @@ public class PlayerHealth : MonoBehaviour
 {	
 	public float health = 100f;					// The player's health.
 	public float repeatDamagePeriod = 2f;		// How frequently the player can be damaged.
-	public AudioClip[] ouchClips;				// Array of clips to play when the player is damaged.
+	//public AudioClip[] ouchClips;				// Array of clips to play when the player is damaged.
 	public float hurtForce = 10f;				// The force with which the player is pushed when hurt.
 	public float damageAmount = 10f;			// The amount of damage to take when enemies touch the player
 
-	private SpriteRenderer healthBar;			// Reference to the sprite renderer of the health bar.
+	//private SpriteRenderer healthBar;			// Reference to the sprite renderer of the health bar.
 	private float lastHitTime;					// The time at which the player was last hit.
 	private Vector3 healthScale;				// The local scale of the health bar initially (with full health).
-	private PlayerControl playerControl;		// Reference to the PlayerControl script.
+	private PlayerControltest3d playerControltest3d;		// Reference to the PlayerControl script.
 	private Animator anim;						// Reference to the Animator on the player
 
 
 	void Awake ()
 	{
 		// Setting up references.
-		playerControl = GetComponent<PlayerControl>();
-		healthBar = GameObject.Find("HealthBar").GetComponent<SpriteRenderer>();
+		playerControltest3d = GetComponent<PlayerControltest3d>();
+		//healthBar = GameObject.Find("HealthBar").GetComponent<SpriteRenderer>();
 		anim = GetComponent<Animator>();
 
 		// Getting the intial scale of the healthbar (whilst the player has full health).
-		healthScale = healthBar.transform.localScale;
+		//healthScale = healthBar.transform.localScale;
 	}
 
 
@@ -47,11 +47,11 @@ public class PlayerHealth : MonoBehaviour
 				else
 				{
 					// Find all of the colliders on the gameobject and set them all to be triggers.
-					Collider2D[] cols = GetComponents<Collider2D>();
+					/*Collider2D[] cols = GetComponents<Collider2D>();
 					foreach(Collider2D c in cols)
 					{
 						c.isTrigger = true;
-					}
+					}*/
 
 					// Move all sprite parts of the player to the front
 					SpriteRenderer[] spr = GetComponentsInChildren<SpriteRenderer>();
@@ -61,10 +61,12 @@ public class PlayerHealth : MonoBehaviour
 					}
 
 					// ... disable user Player Control script
-					GetComponent<PlayerControl>().enabled = false;
+					GetComponent<PlayerControltest3d>().enabled = false;
+
+					Application.LoadLevel (Application.loadedLevel);
 
 					// ... disable the Gun script to stop a dead guy shooting a nonexistant bazooka
-					GetComponentInChildren<Gun>().enabled = false;
+					//GetComponentInChildren<Gun>().enabled = false;
 
 					// ... Trigger the 'Die' animation state
 					anim.SetTrigger("Die");
@@ -77,7 +79,7 @@ public class PlayerHealth : MonoBehaviour
 	void TakeDamage (Transform enemy)
 	{
 		// Make sure the player can't jump.
-		playerControl.jump = false;
+		playerControltest3d.jump = false;
 
 		// Create a vector that's from the enemy to the player with an upwards boost.
 		Vector3 hurtVector = transform.position - enemy.position + Vector3.up * 5f;
@@ -89,20 +91,20 @@ public class PlayerHealth : MonoBehaviour
 		health -= damageAmount;
 
 		// Update what the health bar looks like.
-		UpdateHealthBar();
+		//UpdateHealthBar();
 
 		// Play a random clip of the player getting hurt.
-		int i = Random.Range (0, ouchClips.Length);
-		AudioSource.PlayClipAtPoint(ouchClips[i], transform.position);
+		//int i = Random.Range (0, ouchClips.Length);
+		//AudioSource.PlayClipAtPoint(ouchClips[i], transform.position);
 	}
 
 
-	public void UpdateHealthBar ()
+	/*public void UpdateHealthBar ()
 	{
 		// Set the health bar's colour to proportion of the way between green and red based on the player's health.
 		healthBar.material.color = Color.Lerp(Color.green, Color.red, 1 - health * 0.01f);
 
 		// Set the scale of the health bar to be proportional to the player's health.
 		healthBar.transform.localScale = new Vector3(healthScale.x * health * 0.01f, 1, 1);
-	}
+	}*/
 }
